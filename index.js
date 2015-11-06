39c7e5a8a93d145b75bdc6f3e70058c18c0dc05a
q3boy <q3boy1@gmail.com>
2015-11-06 15:57:42 +0800

    fix coffee/register

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 if (require.extensions['.coffee']) {
-  module.exports = require('./lib/event-pipe.coffee');
+  func = require.extensions['.coffee'].toString();
+  if (0 === func.indexOf('function (module, filename)')) {
+    module.exports = require('./lib/event-pipe.coffee');
+  } else {
+    module.exports = require('./out/release/lib/event-pipe.js');
+  }
 } else {
   module.exports = require('./out/release/lib/event-pipe.js');
 }
